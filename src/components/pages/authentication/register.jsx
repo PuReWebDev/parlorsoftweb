2326ead929224ac492e5ab458aca65b30e0dd4d5
GeorgieSantiago <milestoneartisan@gmail.com>
2019-07-06 21:27:42 -0400

    update registration

--- src/components/pages/authentication/register.jsx
+++ src/components/pages/authentication/register.jsx
@@ -6,54 +6,34 @@ import Col from 'react-bootstrap/Col'
 export const Register = () => (
   <div style={{width: "40%", margin: "0 auto 0 auto", dispay: "block"}}>
   <Card className="text-center">
-    <Card.Header>Login</Card.Header>
+    <Card.Header>Register</Card.Header>
     <Card.Body>
-      <Card.Title>Choose your sign-in option</Card.Title>
+      <Card.Title>Create an account</Card.Title>
   <Form>
     <Form.Row>
       <Form.Group as={Col} controlId="formGridEmail">
         <Form.Label>Email</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
       </Form.Group>
+      </Form.Row>
 
+      <Form.Row>
       <Form.Group as={Col} controlId="formGridPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
+      </Form.Row>
+
+      <Form.Row>
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Label>Confirm Password</Form.Label>
+      <Form.Control type="password" placeholder="Confirm Password" />
+    </Form.Group>
     </Form.Row>
 
-    <Form.Group controlId="formGridAddress1">
-      <Form.Label>Address</Form.Label>
-      <Form.Control placeholder="1234 Main St" />
-    </Form.Group>
-
-    <Form.Group controlId="formGridAddress2">
-      <Form.Label>Address 2</Form.Label>
-      <Form.Control placeholder="Apartment, studio, or floor" />
-    </Form.Group>
-
-    <Form.Row>
-      <Form.Group as={Col} controlId="formGridCity">
-        <Form.Label>City</Form.Label>
-        <Form.Control />
-      </Form.Group>
-
-      <Form.Group as={Col} controlId="formGridState">
-        <Form.Label>State</Form.Label>
-        <Form.Control as="select">
-          <option>Choose...</option>
-          <option>...</option>
-        </Form.Control>
-      </Form.Group>
-
-      <Form.Group as={Col} controlId="formGridZip">
-        <Form.Label>Zip</Form.Label>
-        <Form.Control />
-      </Form.Group>
-    </Form.Row>
 
     <Form.Group id="formGridCheckbox">
-      <Form.Check type="checkbox" label="Check me out" />
+      <Form.Check type="checkbox" label="Send me emails" />
     </Form.Group>
 
     <Button variant="primary" type="submit">
