23ed9ea948d2cbb5eba5f3cd264ca073c5d96abe
Georgie Santiago <gsantiago@C02SH8LCG8WN.amps.local>
2019-09-06 16:56:17 -0400

    correct /routes

--- src/templates/header.js
+++ src/templates/header.js
@@ -10,26 +10,29 @@ import {
 import { Link, Redirect } from 'react-router-dom'
 import { history } from '../api/store'
 
-const SubMenu = ({path}) => {
-    console.log("submenu key",path)
-    switch(path) {
+const SubMenu = ({key}) => {
+    console.log("submenu key",key)
+    if( !key ) {
+      key = "dashboard"
+    }
+    switch(key) {
       case "dashboard":
           return (
             <Nav>
               <NavItem>
-                  <NavLink><Link to="dashboard/stats">Stats</Link></NavLink>
+                  <NavLink><Link to="/dashboard/stats">Stats</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
             </Nav>
           )
@@ -38,19 +41,19 @@ const SubMenu = ({path}) => {
           return (
             <Nav className="ml-auto" navbar>
               <NavItem>
-                  <NavLink><Link to="dashboard/stats">Stats</Link></NavLink>
+                  <NavLink><Link to="/dashboard/stats">Stats</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
             </Nav>
           )
@@ -59,19 +62,19 @@ const SubMenu = ({path}) => {
           return (
             <Nav className="ml-auto" navbar>
               <NavItem>
-                  <NavLink><Link to="dashboard/stats">Stats</Link></NavLink>
+                  <NavLink><Link to="/dashboard/stats">Stats</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
             </Nav>
           )
@@ -80,19 +83,19 @@ const SubMenu = ({path}) => {
           return (
             <Nav className="ml-auto" navbar>
               <NavItem>
-                  <NavLink><Link to="dashboard/stats">Stats</Link></NavLink>
+                  <NavLink><Link to="/dashboard/stats">Stats</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="dashboard/menuitem">Menu item</Link></NavLink>
+                  <NavLink><Link to="/dashboard/menuitem">Menu item</Link></NavLink>
               </NavItem>
             </Nav>
           )
@@ -124,7 +127,7 @@ export const Header = ({isAuth}) => {
               </NavItem>
             </Nav>
           </Collapse>
-          <Redirect to={ pathname } />
+          <Redirect to="/login" />
           </>
           :
           <>
@@ -133,24 +136,27 @@ export const Header = ({isAuth}) => {
           <Collapse isOpen={isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                  <NavLink><Link to="dashboard">Dashboard</Link></NavLink>
+                  <NavLink><Link to="/dashboard">Dashboard</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="clients">Clients</Link></NavLink>
+                  <NavLink><Link to="/clients">Clients</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="mychair">My Chair</Link></NavLink>
+                  <NavLink><Link to="/mychair">My Chair</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="maps">Maps</Link></NavLink>
+                  <NavLink><Link to="/maps">Maps</Link></NavLink>
               </NavItem>
               <NavItem>
-                  <NavLink><Link to="logout">Logout</Link></NavLink>
+                  <NavLink><Link to="/logout">Logout</Link></NavLink>
               </NavItem>
             </Nav>
           </Collapse>
           <br/>
-          <SubMenu path={pathname.replace("/","")} />
+          <Collapse isOpen={isOpen} navbar>
+              <SubMenu key={pathname.replace("/","")} />
+          </Collapse>
+          <Redirect to={ pathname } />
           </>
           }
         </Navbar>
